9ea23b3cbf041e167289709a03947ef96187b281
Aaron Wang <qazxsw523625@gmail.com>
2024-04-13 17:55:46 +0800

    get historical stock data

---
+++ stock_price/historical_stock_price.py
@@ -0,0 +1,40 @@
+import yfinance as yf
+import pandas as pd
+from pymongo.mongo_client import MongoClient
+from config import Config
+import json
+    
+
+
+
+def extract_ticker(tickers, start_date, end_date):
+    for ticker in tickers:
+        data = yf.download(ticker, start=start_date, end=end_date)
+        df = pd.DataFrame(data)
+        for i in range(len(df)):
+            stock_data = {
+                "ticker": ticker,
+                "date": df.iloc[i].name,
+                "open": df.iloc[i]['Open'],
+                "high": df.iloc[i]['High'],
+                "low": df.iloc[i]['Low'],
+                "close": df.iloc[i]['Close'],
+                "volume": df.iloc[i]['Volume']
+            }
+            insert_to_mongo(stock_data)
+    
+
+
+def insert_to_mongo(data):
+    DATABASE_URL = f"mongodb+srv://{Config.MONGODB_USER}:{Config.MONGODB_PASSWORD}@cluster0.ibhiiti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    client = MongoClient(DATABASE_URL)
+    collection = client['TradeChat']['stock_historical_price']
+    if data:
+        collection.insert_one(data)
+    client.close()
+
+
+
+if __name__ == '__main__':
+    tickers = ['AAPL', 'TSLA', 'AAPL', 'NVDA', 'MSFT', 'AMZN', 'META', 'GOOG']
+    extract_ticker(tickers, '2000-01-01', '2024-04-12')
