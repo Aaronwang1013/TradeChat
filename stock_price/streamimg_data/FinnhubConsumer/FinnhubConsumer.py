8bd0cec8c64f839183eea662d14be9d3505fb2ef
Aaron Wang <qazxsw523625@gmail.com>
2024-04-17 15:14:39 +0800

    build streaming stock price data pipeline, finnhub -> kakfa -> spark

--- stock_price/streamimg_data/FinnhubConsumer/FinnhubConsumer.py
+++ stock_price/streamimg_data/FinnhubConsumer/FinnhubConsumer.py
@@ -1,9 +1,56 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
 from pyspark.sql.avro.functions import from_avro
-from confluent_kafka.serialization import StringDeserializer, MessageField
-from confluent_kafka.avro import AvroConsumer
-from confluent_kafka.schema_registry import SchemaRegistryClient
-from confluent_kafka.schema_registry.avro import AvroDeserializer
+from pyspark.sql.functions import col, current_timestamp, concat_ws, expr
+
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+trades_schema = open("trades.avsc", "r").read()
+
+
+def avro_decode(raw_df, schema):
+    """
+    decode avro data from kafka
+    """
+    try:
+        decoded_df = (
+            raw_df.withColumn("value", from_avro("value", schema))
+            .select("value.*")
+            .select('data.c', 'data.p', 'data.s', 'data.t', 'data.v')
+        )
+        return decoded_df
+    except Exception as e:
+        logger.error(f"Error in avro_decode: {e}, raw_df: {raw_df}")
+
+
+
+def parse_df(decoded_df):
+    try:
+        parsed_df = (
+            decoded_df.withColumnRenamed("c", "trade_condition")
+                      .withColumnRenamed("p", "price")
+                      .withColumnRenamed("s", "symbol")
+                      .withColumnRenamed("t", "timestamp")
+                      .withColumnRenamed("v", "volume")
+                      .withColumn("timestamp", explode(col("timestamp")))
+                      .withColumn(
+                "timestamp", (col("timestamp") / 1000).cast("timestamp")
+            )
+            .withColumn("created_at", current_timestamp())
+            .withColumn(
+                "trade_condition",
+                expr("transform(trade_condition, x -> cast(x as string))"),
+            )
+            .withColumn("trade_condition", concat_ws(",", col("trade_condition")))
+        )
+        return parsed_df
+    except Exception as e:
+        logger.error(f"Error in parse_df: {e}, decoded_df: {decoded_df}")
 
 
 
@@ -12,18 +59,17 @@ spark = (
     )
 raw_df = (spark.readStream.format("kafka")
           .option("kafka.bootstrap.servers", "kafka:9092")
-          .option("subscribe", "market").load()
-          .option("startingOffsets", "latest")
+          .option("subscribe", "market")
+          .option("startingOffsets", "latest").load()
         )
 
+decoded_df = avro_decode(raw_df, trades_schema)
+final_df = parse_df(decoded_df)
+query = final_df \
+    .writeStream \
+    .outputMode("append") \
+    .format("console") \
+    .start()
 
-print(raw_df)
 
-
-def avro_decode(data, schema):
-    try:
-        avro_decoded = (
-            data.withColumn("value", from_avro("value", schema))
-        )
-    except Exception as e:
-        print(e)
+query.awaitTermination()
