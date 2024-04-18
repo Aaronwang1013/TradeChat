3a6505f5db996ebb5d913b6834749b76ea2b9b32
Aaron Wang <qazxsw523625@gmail.com>
2024-04-18 12:24:57 +0800

    add twitter sentiment plot api, in progress

--- APP/app.py
+++ APP/app.py
@@ -9,6 +9,10 @@ from flask_bcrypt import check_password_hash
 from flask_bcrypt import generate_password_hash 
 import forms
 from pymongo import MongoClient
+# make plot
+import pandas as pd
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 
 app = Flask(__name__)
@@ -82,6 +86,76 @@ def signin():
         return jsonify({'error': 'Invalid form data'}, 403)
     return render_template('signin.html', form=loginform)
 
+
+def read_twitter_data():
+    DATABASE_URL = f"mongodb+srv://{Config.MONGODB_USER}:{Config.MONGODB_PASSWORD}@cluster0.ibhiiti.mongodb.net/?retryWrites=true&w=secure&appName=Cluster0"
+    client = MongoClient(DATABASE_URL)
+    tweet_collection = client['TradeChat']['Twitter_tweets']
+    stock_collection = client['TradeChat']['Twitter_stock']
+    # Retrieve all the collections
+    tweet_data = list(tweet_collection.find({}))
+    stock_data = list(stock_collection.find({}))
+    tweet_df = pd.DataFrame(tweet_data)
+    stock_df = pd.DataFrame(stock_data)
+    client.close()
+    return tweet_df, stock_df
+
+
+def sentiment_overtime(tweet_df, stock_df, title, score_column_name="score", save_path=None):
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    
+    fig.add_trace(
+        go.Scatter(x=tweet_df['day_date'], y=tweet_df[score_column_name], mode='lines', name=score_column_name),
+        secondary_y=False,
+    )
+
+    fig.add_trace(
+        go.Scatter(x=stock_df['day_date'], y=stock_df['close_value'], mode='lines', name='Stock Price'),
+        secondary_y=True,
+    )
+
+    fig.update_layout(
+        title=f"Effects of {title} tweets to stock price",
+        xaxis_title="Day date",
+        yaxis_title=score_column_name,
+        yaxis2_title="Stock Price",
+    )
+
+    return fig
+
+
+def draw_stock_price_with_sentiment(tweet_df, stock_df, start_day, end_day, score_name="score"):
+    ticker_symbols =  tweet_df["ticker_symbol"].unique()
+    company = pd.read_csv('./dataset/Company.csv')
+    for ticker_symbol in ticker_symbols:
+        print(f"Ticker symbol: {ticker_symbol}")
+
+        sub_company = company[company["ticker_symbol"] == ticker_symbol]["company_name"]
+        if len(sub_company) != 1:
+            continue
+
+        company_name = sub_company.iloc[0]
+
+        print(f"Stock price of {company_name} company with ticker symbol is {ticker_symbol}")
+
+        sub_tweet_df = tweet_df[tweet_df["ticker_symbol"] == ticker_symbol]
+        sub_tweet_df = sub_tweet_df[(sub_tweet_df["day_date"]>=pd.to_datetime(start_day)) & (sub_tweet_df["day_date"]<=pd.to_datetime(end_day))]
+
+        sub_stock_df = stock_df[stock_df["ticker_symbol"] == ticker_symbol]
+        sub_stock_df = sub_stock_df[(sub_stock_df["day_date"]>=pd.to_datetime(start_day)) & (sub_stock_df["day_date"]<=pd.to_datetime(end_day))]
+        save_path = f"./images/{ticker_symbol}_sentiment_overtime.png"
+        fig = sentiment_overtime(sub_tweet_df, sub_stock_df, company_name, score_column_name=score_name, save_path = save_path)
+
+
+
+
+@app.route('/twitter_sentiment', mehotds=['GET', 'POST'])
+def twitter_sentiment():
+    tweet_df, stock_df = read_twitter_data()
+    start_day = min(tweet_df['day_date'])
+    end_day = max(tweet_df['day_date'])
+    fig = draw_stock_price_with_sentiment(tweet_df, stock_df, start_day, end_day)
+    return render_template('twitter_sentiment_form.html', fig=fig)
     
             
 
