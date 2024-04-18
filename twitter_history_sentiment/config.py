b79f40640670d66a89003bee1367e273926e451a
Aaron Wang <qazxsw523625@gmail.com>
2024-04-18 11:47:32 +0800

    store all twitter data in mongodb, use plotly to show plot on front-end

---
+++ twitter_history_sentiment/config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Config:
+
+    MONGODB_USER: str = os.getenv("MONGODB_USER")
+    MONGODB_PASSWORD: str = os.getenv("MONGODB_PASSWORD")
